f98a63159bbd367d34ee4df7ffc79363b7ae41dc
Samantha Johnston <samareimorgan@gmail.com>
2018-08-26 21:45:44 -0600

    Final Commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,55 @@
 $(document).ready(function () {
-  //variables --------
-  var topics = ["Dr. Bensen Honeydew", "Rizzo the Rat", "Scooter", "Statler and Waldorf", "Sweetums", "Lew Zealand", "The Moopets"];
+  //global variables --------
+  var topics = ["Dr. Bensen Honeydew", "Rizzo the Rat", "Scooter", "Statler and Waldorf", "Sweetums", "Lew Zealand", "Cookie Monster"];
+  //Click Events
+  $(document).on("click", createBtn());
+  $(document).on("click", ".muppet-btn", function(event) {
+    muppet = $(this).text();
+    getGiphy();
+    console.log(muppet);
+  })
 
+
+  //Functions---
+  //Create giphy AJAX call function
   function getGiphy() {
-
-    var muppet = $("#muppet-input");
-    var queryURL ="https://api.giphy.com/v1/gifs/search?api_key=umpnfYGGEaxrvpVX15blgnTvZzKpDwyL&q=" + muppet + "&limit=10&offset=0&rating=G&lang=en";
-
+    
+    var queryURL ="https://api.giphy.com/v1/gifs/search?api_key=umpnfYGGEaxrvpVX15blgnTvZzKpDwyL&limit=10&q=" + muppet;
+    console.log(muppet);
     // AJAX call 
     $.ajax({
       url: queryURL,
-      method: "GET"
+      method: "GET" 
     })
       .then(function(response) {
+        //local variable for response data
         var results = response.data;
-
+        console.log(response);
+        //for loop to go through the response data array
         for (var j = 0; j < results.length; j++) {
-          var gifDiv = $("<div class'item'>");
-          
+          // Create div dynamically to hold image and text
+          var gifDiv = $("<div>");
+          // more local variables for the results needed: rating, text and image
           var rating = results[j].rating;
 
           var p = $("<p>").text("Rated: " + rating);
-
+          //creating image tag dynamically with each image - and attributing the src to the image
           var muppetImage = $("<img>");
-          muppetImage.attr("src", results[j].images.fixed_width.url);
+          muppetImage.attr("src", results[j].images.original_still.url);
+          muppetImage.attr("data-animate", results[j].images.fixed_width.url);
+          muppetImage.attr("data-still", results[j].images.original_still.url);
+          muppetImage.attr("data-state","still");
+          muppetImage.addClass("gif");
+          muppetImage.attr("alt", muppet);
+          //append the text and the image to the div created. 
+          gifDiv.append(p);
+          gifDiv.append(muppetImage);
 
-          gifDiv.prepend(p);
-          gifDiv.prepend(muppetImage);
-
-          $("#muppets-view").prepend(gifDiv);
+          //append the div to the muppets-view id
+          $("#muppets-view").append(gifDiv);
         }
       })
-    }
-
-      /*  // Creating a variable to hold the gif img URL
-        var imageUrl = response.data.image_original_url;
-        //Create a varible to create an img tag to hold the image
-        var muppetImage = $("<img>");
-
-        //Add attributes of src and alt to the image
-          muppetImage.attr("src", imageUrl);
-          muppetImage.attr("alt", muppet);
-
-        //prepend images to muppets-view
-        $("#muppets-view").prepend(muppetImage);
-      }); */
-  
-      // Storing the rating data
-     // var gifRating = response.data_rating;
-
-      //  console.log(gifRating);
-
-      // Creating an element to have the rating displayed
-     // var pOne = $("<p>").text("Rating: " + gifRating);f
-
-      // Displaying the rating
-     // muppetDiv.append(pOne);
-
-      // Prepend Gifs to others
-    //  $("#muppets-view").prepend(muppetDiv);
-
-
-  
-
+  }
   //functions and Events
   function createBtn() {
     //Delete items inside the muppets-view div
@@ -73,45 +60,30 @@ $(document).ready(function () {
       //Dynamically create buttons 
       var x = $("<button>");
       x.addClass("muppet-btn");
-      x.attr("data-name", topics[i]);
+      x.attr("data-muppet", topics[i]);
       x.text(topics[i]);
       $("#buttons").append(x);
       }
     }
-
+  // Add input from the muppet-input form to the topics array 
   $("#add-muppet").on("click", function(event) {
     event.preventDefault();
     var muppetInput = $("#muppet-input").val().trim();
     topics.push(muppetInput);
     createBtn();
-    });
+    })
   
+  $(document).on("click", ".gif",function() {
+    console.log("word");
+    var state = $(this).attr("data-state");
+    console.log(state);
+    if(state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else{
+      $(this).attr("src",$(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  })
 
-  $(document).on("click", createBtn());
-  $(document).on("click", ".muppet-btn", getGiphy());
 })
-
-   /* var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=umpnfYGGEaxrvpVX15blgnTvZzKpDwyL&q=muppets&limit=10&offset=0&rating=PG-13&lang=en";
-
-  $.ajax ({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response)
-  }),
-    //("#muppets-view").text(JSON.stringify(response));
-    //$("#muppets-view").html("<h1> Muppet Name: " + response.Title);
-
-  }),  
-  
-    createBtn() */
-  //pseudocode ---
-
-  /*
-  Create a for loop that appends a button to each string of the array.
-
-  On click of the button, 10 images should appear on the screen. 
-  Create a search input that creates a search of Giphy API 
-  Have the query return 10 images relating to the search and the rating of the image. 
-  The gifs should appear in pause position.  
-  When clicked the gif needs to animate. */
